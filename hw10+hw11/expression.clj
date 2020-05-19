b3892790708cef5214c2e540210531751222a6b2
Tanya Cheremkhina <53428519+tcheremkhina@users.noreply.github.com>
2020-05-20 00:17:45 +0300

    Update expression.clj

--- hw10+hw11/expression.clj
+++ hw10+hw11/expression.clj
@@ -1,5 +1,5 @@
 (defn expression [func] (fn [& args] (fn [values] (apply func (mapv (fn [i] (i values)) args)))))
-(defn constant [x] (fn [values] x))
+(defn constant [x] (constantly x))
 (defn variable [x] (fn [values] (get values x)))
 (def add (expression +))
 (def subtract (expression -))
@@ -8,22 +8,22 @@
 (def med (expression (fn [& args] (nth (sort args) (long (/ (count args) 2))))))
 (def avg (expression (fn [& args] (/ (apply + args) (count args)))))
 (def divide (expression (fn [& args] (/ (double (first args)) (apply * (drop 1 args))))))
-(def oper {'+ add '- subtract '* multiply '/ divide 'negate negate 'med med 'avg avg})
+(def oper {'+ add '- subtract '* multiply '/ divide 'negate negate 'med med 'avg avg 'const constant 'var variable})
 (def vars {'x "x" 'y "y" 'z "z"})
-(defn parse [args] (cond
-                     (= (type args) clojure.lang.PersistentList)
-                     (let [newArgs (mapv (fn [i] (parse i)) (drop 1 args))
-                           exp (get oper (first args))] (apply exp newArgs))
-                     (isa? (type args) Number) (constant args)
-                     :else (variable (get vars args)))
-  )
-(defn parseFunction [input] (parse (read-string input)))
+(defn parse [exprs] (defn toParse [args] (cond
+                                           (= (type args) clojure.lang.PersistentList)
+                                           (let [newArgs (mapv (fn [i] (toParse i)) (drop 1 args))
+                                                 exp (get exprs (first args))] (apply exp newArgs))
+                                           (isa? (type args) Number) ((get exprs 'const) args)
+                                           :else ((get exprs 'var) (get vars args)))))
+
+(defn parseFunction [input] ((parse oper) (read-string input)))
 ;hw10 -----------------> hw11
 (defn proto-get [obj key]
   (cond (contains? obj key) (obj key)
         (contains? obj :prototype) (proto-get (obj :prototype) key)))
 (defn proto-call [this key & args]
-  (apply (proto-get this key) this args))
+  (force (apply (proto-get this key) this args)))
 (defn field [key]
   (fn [this] (proto-get this key)))
 (defn method [key]
@@ -36,61 +36,56 @@
 (def _f (field :f))
 (def _args (field :args))
 (def _oper (field :oper))
-(defn Constant [])
-(defn Variable [])
+
+(def Constant)
+(def Variable)
 (def ConstantProto {
-                    :evaluate (fn [this vars] (_x this))
-                    :toString (fn [this] (format "%.1f" (_x this))) ;(format "%.1f" value)
-                    :diff     (fn [this d] (Constant 0))
+                    :evaluate (fn [this vars] (first (_args this)))
+                    :toString (fn [this] (format "%.1f" (double (first (_args this)))))
+                    :diff     (constantly (delay (Constant 0)))
                     })
 (def VariableProto {
                     :evaluate (fn [this vars] (get vars (toString this)))
-                    :toString (fn [this] (str (_x this)))
-                    :diff     (fn [this d] (cond (= d (_x this)) (Constant 1)
-                                                 :else (Constant 0)))})
-(defn Constant [x] {
-                    :x         x
-                    :prototype ConstantProto})
-(defn Variable [x] {
-                    :x         x
-                    :prototype VariableProto})
+                    :toString (fn [this] (str (first (_args this))))
+                    :diff     (fn [this d] (if (= d (first (_args this))) (Constant 1) (Constant 0)))
+                    })
 (def ExpressionProto {
                       :evaluate (fn [this vars] (apply (_f this) (mapv #(evaluate % vars) (_args this))))
-                      :toString (fn [this]                  ;                                  (cond (= (count (_args this)) 1) (toString (first (_args this)))
+                      :toString (fn [this]
                                   (str "(" (_oper this) " " (clojure.string/join " " (mapv toString (_args this))) ")"))
                       })
-(defn constructor [cons proto] (fn [& args] (apply cons {:prototype proto} args)))
 (defn ExprConstructor [this & args] (assoc this :args args))
+(defn constructor [proto] (fn [& args] (apply ExprConstructor {:prototype proto} args)))
 (defn makeProto [f oper dF] {
                              :prototype ExpressionProto
                              :f         f
                              :oper      oper
                              :diff      (fn [this d]
                                           (cond (= (count (_args this)) 1)
-                                                ((constructor ExprConstructor (makeProto f oper dF)) (diff (first (_args this)) d))
+                                                ((constructor (makeProto f oper dF)) (diff (first (_args this)) d))
                                                 :else (dF this d)))
                              })
-
-(def Add (constructor ExprConstructor (makeProto + "+" (fn [this d] (apply Add (mapv #(diff % d) (_args this)))))))
-(def Subtract (constructor ExprConstructor (makeProto - "-" (fn [this d] (apply Subtract (mapv #(diff % d) (_args this)))))))
-(def Multiply (constructor ExprConstructor (makeProto * "*" (fn [this d] (let [u (first (_args this)) v (apply Multiply (drop 1 (_args this)))]
-                                                                           (Add (Multiply (diff u d) v)
-                                                                                (Multiply (diff v d) u)))))))
-(defn div [this vars] (let [args (_args this) u (evaluate (first args) vars) v (evaluate (apply Multiply (drop 1 args)) vars)] (/ (double u) v)))
+(def Constant (constructor ConstantProto))
+(def Variable (constructor VariableProto))
+(def Add (constructor (makeProto + "+" (fn [this d] (apply Add (mapv #(diff % d) (_args this)))))))
+(def Subtract (constructor (makeProto - "-" (fn [this d] (apply Subtract (mapv #(diff % d) (_args this)))))))
+(def Multiply (constructor (makeProto * "*" (fn [this d]
+                                              (let [u (first (_args this)) v (apply Multiply (drop 1 (_args this)))]
+                                                (Add (Multiply (diff u d) v) (Multiply (diff v d) u)))))))
+(defn div [& x] (/ (double (first x)) (apply * (drop 1 x))))
 (def Divide)
-(def DivideProto (makeProto / "/" (fn [this d] (let [u (first (_args this)) v (apply Multiply (drop 1 (_args this)))]
+(def DivideProto (makeProto div "/" (fn [this d] (let [u (first (_args this)) v (apply Multiply (drop 1 (_args this)))]
                                                  (Divide (Subtract (Multiply (diff u d) v) (Multiply (diff v d) u))
                                                          (Multiply v v))))))
-(def DivideProto (assoc DivideProto :prototype (assoc (DivideProto :prototype) :evaluate div)))
-(def Divide (constructor ExprConstructor DivideProto))
-(def Negate (constructor ExprConstructor (makeProto - "negate" (fn [this d] (Negate (diff (first (_args this)) d))))))
-(def exprs {'+ Add '- Subtract '* Multiply '/ Divide 'negate Negate})
+(def Divide (constructor DivideProto))
+(def Negate (constructor (makeProto - "negate" (fn [this d] (Negate (diff (first (_args this)) d))))))
+(def Sum (constructor (makeProto + "sum" (fn [this d] (apply Sum (mapv #(diff % d) (_args this)))))))
+(defn avgF [& x] (/ (apply + x) (count x)))
+(def Avg)
+(def AvgProto (makeProto avgF "avg"
+                         (fn [this d] (let [args (_args this) sum (apply Add args) n (Constant (count args))]
+                                        (Divide (diff sum d) n)))))
+(def Avg (constructor AvgProto))
 
-(defn parseObj [args] (cond
-                        (= (type args) clojure.lang.PersistentList)
-                        (let [newArgs (mapv (fn [i] (parseObj i)) (drop 1 args))
-                              expr (get exprs (first args))] (apply expr newArgs))
-                        (isa? (type args) Number) (Constant args)
-                        :else (Variable (get vars args))))
-
-(defn parseObject [input] (parseObj (read-string input)))
+(def exprs {'+ Add 'sum Sum 'avg Avg '- Subtract '* Multiply '/ Divide 'negate Negate 'var Variable 'const Constant})
+(defn parseObject [input] ((parse exprs) (read-string input)))
