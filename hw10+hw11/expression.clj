e02a242c65edc88021735db32ca95aeac9ce1a58
Tanya Cheremkhina <53428519+tcheremkhina@users.noreply.github.com>
2020-06-04 17:58:29 +0300

    Update expression.clj
    final version

--- hw10+hw11/expression.clj
+++ hw10+hw11/expression.clj
@@ -36,56 +36,60 @@
 (def _f (field :f))
 (def _args (field :args))
 (def _oper (field :oper))
-
 (def Constant)
 (def Variable)
-(def ConstantProto {
-                    :evaluate (fn [this vars] (first (_args this)))
-                    :toString (fn [this] (format "%.1f" (double (first (_args this)))))
-                    :diff     (constantly (delay (Constant 0)))
-                    })
-(def VariableProto {
-                    :evaluate (fn [this vars] (get vars (toString this)))
-                    :toString (fn [this] (str (first (_args this))))
-                    :diff     (fn [this d] (if (= d (first (_args this))) (Constant 1) (Constant 0)))
-                    })
+(defn Constant [& args] {
+                         :evaluate (fn [this vars] (first (_args this)))
+                         :toString (fn [this] (format "%.1f" (double (first (_args this)))))
+                         :diff     (constantly (delay (Constant 0)))
+                         :args     args
+                         })
+(def ZERO (Constant 0))
+(def ONE (Constant 1))
+(defn Variable [& args] {
+                         :evaluate (fn [this vars] (get vars (toString this)))
+                         :toString (fn [this] (str (first (_args this))))
+                         :args     args
+                         :diff     (fn [this d] (if (= d (first (_args this))) ONE ZERO))
+                         })
 (def ExpressionProto {
                       :evaluate (fn [this vars] (apply (_f this) (mapv #(evaluate % vars) (_args this))))
                       :toString (fn [this]
                                   (str "(" (_oper this) " " (clojure.string/join " " (mapv toString (_args this))) ")"))
                       })
-(defn ExprConstructor [this & args] (assoc this :args args))
-(defn constructor [proto] (fn [& args] (apply ExprConstructor {:prototype proto} args)))
-(defn makeProto [f oper dF] {
-                             :prototype ExpressionProto
-                             :f         f
-                             :oper      oper
-                             :diff      (fn [this d]
-                                          (cond (= (count (_args this)) 1)
-                                                ((constructor (makeProto f oper dF)) (diff (first (_args this)) d))
-                                                :else (dF this d)))
-                             })
-(def Constant (constructor ConstantProto))
-(def Variable (constructor VariableProto))
-(def Add (constructor (makeProto + "+" (fn [this d] (apply Add (mapv #(diff % d) (_args this)))))))
-(def Subtract (constructor (makeProto - "-" (fn [this d] (apply Subtract (mapv #(diff % d) (_args this)))))))
-(def Multiply (constructor (makeProto * "*" (fn [this d]
-                                              (let [u (first (_args this)) v (apply Multiply (drop 1 (_args this)))]
-                                                (Add (Multiply (diff u d) v) (Multiply (diff v d) u)))))))
-(defn div [& x] (/ (double (first x)) (apply * (drop 1 x))))
-(def Divide)
-(def DivideProto (makeProto div "/" (fn [this d] (let [u (first (_args this)) v (apply Multiply (drop 1 (_args this)))]
-                                                 (Divide (Subtract (Multiply (diff u d) v) (Multiply (diff v d) u))
-                                                         (Multiply v v))))))
-(def Divide (constructor DivideProto))
-(def Negate (constructor (makeProto - "negate" (fn [this d] (Negate (diff (first (_args this)) d))))))
-(def Sum (constructor (makeProto + "sum" (fn [this d] (apply Sum (mapv #(diff % d) (_args this)))))))
-(defn avgF [& x] (/ (apply + x) (count x)))
-(def Avg)
-(def AvgProto (makeProto avgF "avg"
-                         (fn [this d] (let [args (_args this) sum (apply Add args) n (Constant (count args))]
-                                        (Divide (diff sum d) n)))))
-(def Avg (constructor AvgProto))
+(defn ExprConstructor [this & args] (assoc this
+                                      :args args))
 
+(defn constructor [proto] (fn [& args] (apply ExprConstructor {:prototype proto} args)))
+(defn makeProto [f oper dF] (constructor {
+                                          :prototype ExpressionProto
+                                          :f         f
+                                          :oper      oper
+                                          :diff      (fn [this d] (cond (= (count (_args this)) 1)
+                                                        ((makeProto f oper dF) (diff (first (_args this)) d))
+                                                        :else (dF (_args this)
+                                                                  (mapv #(diff % d) (_args this))
+                                                                  d)))
+                                          }))
+
+(def Add (makeProto + "+" (fn [x x' d] (apply Add x'))))
+(def Subtract (makeProto - "-" (fn [x x' d] (apply Subtract x'))))
+(def Multiply (makeProto * "*" (fn [x x' d] (let [u' (first x')
+                                                  u (first x)
+                                                  v (apply Multiply (drop 1 x))
+                                                  v' (diff v d)]
+                                              (Add (Multiply u' v) (Multiply v' u))))))
+(defn div [& x] (/ (double (first x)) (apply * (drop 1 x))))
+(def Divide (makeProto div "/" (fn [x x' d] (let [u' (first x')
+                                                  u (first x)
+                                                  v (apply Multiply (drop 1 x))
+                                                  v' (diff v d)]
+                                              (Divide (Subtract (Multiply u' v) (Multiply v' u)) (Multiply v v))))))
+(def Negate (makeProto - "negate" (fn [])))
+(def Sum (makeProto + "sum" (fn [x x' d] (apply Sum x'))))
+(defn avgF [& x] (/ (apply + x) (count x)))
+(def Avg (makeProto avgF "avg" (fn [x x' d] (let [sum' (apply Sum x')
+                                                  n (Constant (count x))]
+                                   (Divide sum' n)))))
 (def exprs {'+ Add 'sum Sum 'avg Avg '- Subtract '* Multiply '/ Divide 'negate Negate 'var Variable 'const Constant})
 (defn parseObject [input] ((parse exprs) (read-string input)))
